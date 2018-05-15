570c9fb2d0c05122a50a143532302bd853ed1082
Jack-Harding1 <jackhardingwork@gmail.com>
2018-05-15 21:15:23 +0200

    embedalign

---
+++ 2-learning_word_reps/src/Embed_Align_Net.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 21:08:52 2018
+
+@author: jackharding
+"""
+
---
+++ 2-learning_word_reps/src/Embed_Align_parameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 21:10:35 2018
+
+@author: jackharding
+"""
+
